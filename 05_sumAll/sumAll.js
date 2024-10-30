f011c4e56b3581fec4ebfacfa3bdabe347093dbe
mballach <muggins111@live.ca>
2024-10-30 14:17:16 -0700

    Complete exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    let retVal = 0;
+    if (!Number.isInteger(intA)){
+        return "ERROR"
+    } else if (!Number.isInteger(intB) || intA<0 || intB<0){
+        return "ERROR"
+    } else if (intA>=intB){
+        while (intB<intA+1){
+            retVal+=intB;
+            intB++;
+        }
+        return retVal;
+    } else {
+        while (intA<intB+1){
+            retVal+=intA;
+            intA++;
+        }
+        return retVal;
+    }
 };
 
 // Do not edit below this line
